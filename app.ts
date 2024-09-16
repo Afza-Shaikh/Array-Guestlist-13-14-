72a8e13d39d99aab83a9eed7bf504c21e8b8b282
afza shaikh <shaikhafza961@gmail.com>
2024-09-16 22:03:13 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,4 @@
+let transportation: string  [] = ['civic','bike','suzuki jis mai cow ho'];
+for(let i=0; i < transportation.length; i++){
+    console.log('I would like to own a '+ transportation[i]);
+}
